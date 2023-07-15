950e0aa32888522a952ef88f2921b2cf691b4744
Ritish Bhatoye <ritishrahul87@gmail.com>
2023-07-15 22:48:48 +0530

    JS

---
+++ JS.js
@@ -0,0 +1,17 @@
+const fileInput=document.getElementById('input');
+/*function func()
+{
+    alert("Thank You For Subscribing");
+}*/
+function func()
+{
+    if(fileInput[0]==' ')
+    {
+        alert("Please enter your email")
+    }
+    else
+{
+    const a=document.getElementById('fd').innerHTML="Thank You For Subscribing";
+document.write(a);
+}
+}
